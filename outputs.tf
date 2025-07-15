c2097c4131f8467832b1348c77c355647d4fbb4f
zhang make <zhangmake86@gmail.com>
2025-07-15 13:00:13 +0100

    feat: added name of function to outputs

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,8 @@ output "invoke_arn" {
   value = aws_lambda_function.lambda_func.invoke_arn
   description = "The Invoke ARN of the Lambda function"
 }
+
+output "function_name" {
+  value = aws_lambda_function.lambda_func.function_name
+  description = "The name of the function"
+}
